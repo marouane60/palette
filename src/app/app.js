1d9fc9e16e5219a71e36c1f4c727abc58a8e3ae7
marouane60 <marwan.hilal60@gmail.com>
2019-05-05 16:59:51 +0200

    added firebase signup

--- src/app/app.js
+++ src/app/app.js
@@ -31,6 +31,7 @@ angular.module('nuBoard', ['firebase', 'ngRoute'])
         templateUrl: 'app/app.tpl.html'
       })
       .when('/sign-in', {
+        controller: 'SignCtrl',
         templateUrl: 'app/sign-in/sign-in.tpl.html'
       })
       .otherwise({
@@ -56,4 +57,41 @@ angular.module('nuBoard', ['firebase', 'ngRoute'])
     $scope.surfacePositionOffset = {};
   })
 
+  .controller('SignCtrl', function ($scope, $routeParams, $rootScope, SyncService, AppConfig) {
+    
+      // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBUDgl99tdUHmvUuISj4van0SCuUA_cX6g",
+    authDomain: "palette-ef61a.firebaseapp.com",
+    databaseURL: "https://palette-ef61a.firebaseio.com",
+    projectId: "palette-ef61a",
+    storageBucket: "palette-ef61a.appspot.com",
+    messagingSenderId: "831285475315",
+    appId: "1:831285475315:web:956b2a6f103134ea"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+      // FirebaseUI config.
+      var uiConfig = {
+        signInSuccessUrl: '<url-to-redirect-to-on-success>',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        // tosUrl and privacyPolicyUrl accept either url string or a callback
+        // function.
+        // Terms of service url/callback.
+        tosUrl: '<your-tos-url>',
+        // Privacy policy url/callback.
+        privacyPolicyUrl: function() {
+          window.location.assign('<your-privacy-policy-url>');
+        }
+      };
+
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
+  })
   
