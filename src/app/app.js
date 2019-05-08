3ec0bf5017830952dc95bc87a0fafab625b78b19
marouane60 <marwan.hilal60@gmail.com>
2019-05-08 17:19:09 +0200

    added users table

--- src/app/app.js
+++ src/app/app.js
@@ -72,9 +72,46 @@ angular.module('nuBoard', ['firebase', 'ngRoute'])
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  // Get a reference to the database service
+  var database = firebase.database();
+
       // FirebaseUI config.
       var uiConfig = {
-        signInSuccessUrl: '<url-to-redirect-to-on-success>',
+        callbacks: {
+          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            var user = authResult.user;
+            var credential = authResult.credential;
+            var isNewUser = authResult.additionalUserInfo.isNewUser;
+            var providerId = authResult.additionalUserInfo.providerId;
+            var operationType = authResult.operationType;
+
+            console.log("user : "+user+"credential : "+credential+"isnewuser : "+isNewUser+"providerid : "+providerId
+            +"operationtype : "+operationType);
+
+            if(isNewUser){
+                firebase.database().ref('users/' + authResult.user.uid).set({
+                  id:authResult.user.uid,
+                  email: authResult.user.email,
+                  inkLvl:100
+                });
+              
+
+
+            }
+            // Do something with the returned AuthResult.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+          },
+          signInFailure: function(error) {
+            // Some unrecoverable error occurred during sign-in.
+            // Return a promise when error handling is completed and FirebaseUI
+            // will reset, clearing any UI. This commonly occurs for error code
+            // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
+            // occurs. Check below for more details on this.
+            return handleUIError(error);
+          }
+        },
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.EmailAuthProvider.PROVIDER_ID
